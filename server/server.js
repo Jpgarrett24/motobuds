c797e7908341f797e8afb841bc35e88f98b8508a
jpgarrett <jpgarrett24@gmail.com>
2021-03-01 13:40:10 -0800

    Set up connection to MongoDB using mongoose

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
-const express = require('express');
-const app = express();
 const port = 8000;
+const db_name = 'motobuds_db';
+const express = require('express');
+const cors = require('cors');
+
+require('./config/mongoose.config')(db_name);
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
