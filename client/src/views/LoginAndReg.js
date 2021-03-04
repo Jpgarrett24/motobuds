367ca911c043fe36a078fccef614584b84a4bba7
jpgarrett <jpgarrett24@gmail.com>
2021-03-04 15:44:05 -0800

    Initial setup for front-end development. Installed node packages, set up iniital components and views

---
+++ client/src/views/LoginAndReg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/motobuds_logo.png';
+
+const LoginAndReg = () => {
+    return (
+        <>
+            <main>
+            </main>
+        </>
+    );
+};
+
+export default LoginAndReg;
