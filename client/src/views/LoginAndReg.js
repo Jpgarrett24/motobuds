d22bd93a316e82d39ecbf97f002f6d99784bdbd9
jpgarrett <jpgarrett24@gmail.com>
2021-03-04 18:47:16 -0800

    Sign In form styling complete for desktop

--- client/src/views/LoginAndReg.js
+++ client/src/views/LoginAndReg.js
@@ -32,9 +32,16 @@ const LoginAndReg = () => {
                     <form action="">
                         <label htmlFor="email">Email</label>
                         <input name="email" id="email" type="email" placeholder="example@email.com" />
-                        <label htmlFor="">Password</label>
+                        <label htmlFor="password">Password</label>
                         <input name="password" id="password" type={password} placeholder="password" />
-                        <span>{password === 'password' ? <FaEye className="passwordVisible" /> : <FaEyeSlash className="passwordVisible" />}</span>
+                        <span>
+                            {password === 'password' ?
+                                <FaEye className="passwordVisible" onClick={() => setPassword('text')} /> :
+                                <FaEyeSlash className="passwordVisible" onClick={() => setPassword('password')} />}
+                        </span>
+                        <div>
+                            <button>Log In</button>
+                        </div>
                     </form>
                 </section>
             </main>
