d8d94a9cf29b44ebdf504d9801096f67bc306a47
jpgarrett <jpgarrett24@gmail.com>
2021-03-11 22:49:16 -0800

    Leave Ride button functional. Can encapsulate functionality into tripsApi file.

--- client/src/views/TripDetails.js
+++ client/src/views/TripDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FaUserPlus } from 'react-icons/fa';
+import { FaUserPlus, FaUserMinus } from 'react-icons/fa';
 
 import Footer from '../components/Footer';
 import { formatDate } from '../helper/formatDate';
@@ -11,12 +11,16 @@ const TripDetails = ({ match }) => {
     const [trip, setTrip] = useState();
     const [date, setDate] = useState();
     const [loading, setLoading] = useState(true);
+    const [joined, setJoined] = useState(false);
     const auth = useAuth();
 
     const getTrip = async (_id) => {
         let result = await tripsApi.getOne(_id);
         setTrip(result.data);
         setDate(formatDate(result.data.startDate));
+        result.data.riders.forEach((rider) => {
+            if (rider._id === auth.user._id) return setJoined(true);
+        });
     };
 
     const joinRide = async () => {
@@ -27,6 +31,16 @@ const TripDetails = ({ match }) => {
         updatedRiders.riders.push(auth.user._id);
         tripsApi.joinRide(trip._id, updatedRiders);
         setLoading(true);
+    };
+
+    const leaveRide = async () => {
+        let updatedRiders = {
+            "riders": trip.riders.filter((rider) => rider._id !== auth.user._id)
+        };
+        updatedRiders.riders = updatedRiders.riders.map((rider) => rider._id);
+        tripsApi.joinRide(trip._id, updatedRiders);
+        setLoading(true);
+        setJoined(false);
     };
 
     useEffect(() => {
@@ -44,7 +58,7 @@ const TripDetails = ({ match }) => {
                 <div id="backgroundLogo" />
                 {loading ? <></> :
                     <>
-                        <button onClick={joinRide}><FaUserPlus id="joinLogo" /><span>Join ride</span></button>
+                        {joined ? <button onClick={leaveRide} id="leaveRide"><FaUserMinus className="joinLeave" /><span>Leave ride</span></button> : <button onClick={joinRide}><FaUserPlus className="joinLeave" /><span>Join ride</span></button>}
                         <h1>{trip.from.city} to {trip.to.city}</h1>
                         <h2>{date.date} {date.time}</h2>
                         <p>Riders:</p>
