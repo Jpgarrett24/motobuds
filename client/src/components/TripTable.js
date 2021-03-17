5186b1407a17348fbf01d616849f7d7198b7ef14
jpgarrett <jpgarrett24@gmail.com>
2021-03-17 12:22:26 -0700

    Users can now search for a specific starting or end location

--- client/src/components/TripTable.js
+++ client/src/components/TripTable.js
@@ -1,35 +1,50 @@
-import React from 'react'
-import { FaUserFriends } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react'
+import { FaUserFriends, FaSearchLocation } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import { formatDate } from '../helper/formatDate';
 
 const TripTable = ({ trips }) => {
+    const [search, setSearch] = useState('');
+    const [tripsList, setTripsList] = useState(trips);
+
+    useEffect(() => {
+        return setTripsList(
+            trips.filter((trip) => trip.from.city.includes(search) || trip.to.city.includes(search))
+        );
+    }, [search, trips])
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Start</th>
-                    <th>End</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th><FaUserFriends size={30} id="numRiders" /></th>
-                </tr>
-            </thead>
-            <tbody>
-                {trips.map((trip) =>
-                    <Link to={`/rides/${trip._id}`} key={trip._id}>
-                        <tr>
-                            <td>{trip.from.city}</td>
-                            <td>{trip.to.city}</td>
-                            <td>{formatDate(trip.startDate).date}</td>
-                            <td>{formatDate(trip.startDate).time}</td>
-                            <td>{trip.riders.length}</td>
-                        </tr>
-                    </Link>
-                )}
-            </tbody>
-        </table>
+        <>
+            <div id="searchBar">
+                <label htmlFor="search"><FaSearchLocation /></label>
+                <input type="text" id="search" placeholder="San Jose" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Meeting Location</th>
+                        <th>End</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th><FaUserFriends size={30} id="numRiders" /></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tripsList.map((trip) =>
+                        <Link to={`/rides/${trip._id}`} key={trip._id}>
+                            <tr>
+                                <td>{trip.from.city}</td>
+                                <td>{trip.to.city}</td>
+                                <td>{formatDate(trip.startDate).date}</td>
+                                <td>{formatDate(trip.startDate).time}</td>
+                                <td>{trip.riders.length}</td>
+                            </tr>
+                        </Link>
+                    )}
+                </tbody>
+            </table>
+        </>
     );
 };
 
